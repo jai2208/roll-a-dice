13da06bc0d311b4cc53bb498620a3829c6edc519
jai2208 <85051833+jai2208@users.noreply.github.com>
2022-12-05 14:04:01 +0530

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,42 @@
+import random
+
+
+response = input("write y to start and n to exit:")
+
+while response == "y" :
+    no = random.randint(1,6)
+    
+    if response == "y":
+        print(no)
+        response = input("write y to start and n to exit:")
+
+
+
+        
+    
+       
+        
+
+    
+
+
+
+    
+
+
+     
+
+
+    
+
+     
+    
+    
+   
+    
+
+   
+
+
+
+   
